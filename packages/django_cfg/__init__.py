67d7324f143285e0b5619d00d3a90e8e75f15a1f
Mark <markolofsen@gmail.com>
2025-10-30 17:34:51 +0800

    feat(django_admin): Add declarative resource configuration and background task support
    - Auto-generate a ModelResource class for import/export, using ResourceConfig if provided, otherwise generating a basic Resource
    - Apply exclusions if specified in the ResourceConfig
    - Add ResourceConfig settings to the Resource class Meta attributes
    - Introduce ResourceConfig for declarative configuration of import/export behavior
    - Introduce BackgroundTaskConfig for configuring background task processi...

--- packages/django_cfg/__init__.py
+++ packages/django_cfg/__init__.py
@@ -32,7 +32,7 @@ Example:
 default_app_config = "django_cfg.apps.DjangoCfgConfig"
 
 # Version information
-__version__ = "1.4.106"
+__version__ = "1.4.107"
 __license__ = "MIT"
 
 # Import registry for organized lazy loading
