77abbd9f4ea31ee55babfe2a9ac1d7be3436a877
Mark <markolofsen@gmail.com>
2025-11-02 21:03:04 +0800

    feat: bump version to 1.5.3

--- packages/django_cfg/__init__.py
+++ packages/django_cfg/__init__.py
@@ -32,7 +32,7 @@ Example:
 default_app_config = "django_cfg.apps.DjangoCfgConfig"
 
 # Version information
-__version__ = "1.5.2"
+__version__ = "1.5.3"
 __license__ = "MIT"
 
 # Import registry for organized lazy loading
