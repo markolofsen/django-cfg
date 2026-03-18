90283d9e652e0b4cabf79eb97da5cb017da8ddc9
Mark <markolofsen@gmail.com>
2026-03-18 22:09:04 +0800

    chore(django-cfg): bump version to 2.0.7

--- django_cfg/__init__.py
+++ django_cfg/__init__.py
@@ -32,7 +32,7 @@ Example:
 default_app_config = "django_cfg.apps.DjangoCfgConfig"
 
 # Version information
-__version__ = "2.0.6"
+__version__ = "2.0.7"
 __license__ = "MIT"
 
 # Setup warnings debug early (checks env var only at this point)
