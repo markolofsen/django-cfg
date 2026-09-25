5b3e094f2e6074fda6e44a6ac34fcd3966e3b9e8
Mark <markolofsen@gmail.com>
2026-09-25 12:01:25 +0800

    sync release 2.3.319

--- django_cfg/__init__.py
+++ django_cfg/__init__.py
@@ -32,7 +32,7 @@ Example:
 default_app_config = "django_cfg.apps.DjangoCfgConfig"
 
 # Version information
-__version__ = "2.3.317"
+__version__ = "2.3.319"
 __license__ = "MIT"
 
 # Setup warnings debug early (checks env var only at this point)
